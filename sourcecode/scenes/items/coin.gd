331a0c79064a6159d9f3fcd8965c18fb0b399822
IN57KC <torsten.wiederkehr@ing.com>
2026-06-26 18:41:18 +0200

    Coin can be colleted

--- sourcecode/scenes/items/coin.gd
+++ sourcecode/scenes/items/coin.gd
@@ -1,6 +1,45 @@
 class_name Coin
 extends Area2D
 
+var Constants = preload("res://scripts/constants.gd")
 
-func _ready():
-	pass # Replace with function body.
+# Export: Item-Typ wird im Editor gesetzt
+@export var item_type: int = 0
+
+# Sprite
+@onready var sprite = $Sprite2D
+
+# Animation
+var bob_speed: float = 2.0
+var bob_amount: float = 10.0
+var starting_y: float = 0.0
+var rotation_speed: float = 0.0
+var elapsed_time: float = 0.0
+
+# Signals
+signal collected(item_type: int, position: Vector2)
+
+
+func _ready() -> void:
+	# Zu "items" Gruppe hinzufügen für schnelle Erkennung
+	add_to_group("items")
+	
+	# Startposition speichern für bobbing animation
+	starting_y = global_position.y
+	
+
+
+func _process(delta: float) -> void:
+	# Animation: auf und ab bobben + rotieren
+	elapsed_time += delta
+	
+	# Y-Position bobben
+	global_position.y = starting_y + sin(elapsed_time * bob_speed) * bob_amount
+	
+	# Rotieren
+	rotation += rotation_speed * delta
+
+
+func _on_item_collected() -> void:
+	emit_signal("collected", item_type, global_position)
+	queue_free()
